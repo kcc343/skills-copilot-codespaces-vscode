f788203f5d211c96fda9a4eba2d6a801e016ba7c
Kelly C <31262826+kcc343@users.noreply.github.com>
2024-06-09 18:16:45 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,11 @@
+function skillsMember()
+{
+    // Set the member's skills
+    this.skills = [
+        { name: "HTML", level: 3 },
+        { name: "CSS", level: 2 },
+        { name: "JavaScript", level: 2 },
+        { name: "PHP", level: 1 },
+        { name: "MySQL", level: 1 }
+    ];
+}
